d845b1d5f7468a5082a1f5f3aa0b113585c45cd7
KirillKozlov <kirillandreevickozlov@yandex.ru>
2025-10-15 22:01:26 +0300

    template

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from services.wb_api_report_class import WBApiReportByPeriodClass
 from dotenv import load_dotenv
 import os 
 
-
 def main():
-    parser = WBApiReportByPeriodClass(WB_TOKEN, URL_REPORT_DETAIL_BY_PERIOD)
+    parser = WBApiReportByPeriodClass(NM_ID, LIMIT_NUMBER, WB_TOKEN, URL_REPORT_DETAIL_BY_PERIOD)
     parser.parse_spp()
 
 if __name__ == "__main__":
     load_dotenv()
+    NM_ID = int(os.getenv("NM_ID_INT"))
+    LIMIT_NUMBER = int(os.getenv("LIMIT_NUMBER_INT"))
     WB_TOKEN = os.getenv("WB_TOKEN_STR")
     URL_REPORT_DETAIL_BY_PERIOD = os.getenv("URL_REPORT_DETAIL_BY_PERIOD_STR")
     main()
-
--- services/wb_api_report_class.py
+++ services/wb_api_report_class.py
@@ -1,17 +1,22 @@
 import requests
+import json
 
 class WBApiReportByPeriodClass():
-    def __init__(self, WB_TOKEN: str, URL_REPORT_DETAIL_BY_PERIOD: str):
+    def __init__(self, NM_ID: int, LIMIT_NUMBER: int, WB_TOKEN: str, URL_REPORT_DETAIL_BY_PERIOD: str):
         self.WB_TOKEN = WB_TOKEN
         self.URL_REPORT_DETAIL_BY_PERIOD = URL_REPORT_DETAIL_BY_PERIOD
         self.HEADERS = {
             "Authorization": self.WB_TOKEN
         }
         self.PARAMS = {
-            "dateFrom": "2025-10-14",
-            "dateTo": "2025-10-15",
-            "period": "daily"
+            "limit": LIMIT_NUMBER,
+            "filterNmID": NM_ID
         }
     def parse_spp(self):
         response = requests.get(self.URL_REPORT_DETAIL_BY_PERIOD, params=self.PARAMS, headers=self.HEADERS)
-        print(response.status_code)
+        if response.status_code == 200:
+            data_json = response.json()
+            print(data_json)
+            with open("data.json", "w", encoding="utf-8") as f:
+                json.dump(data_json, f, indent=4, ensure_ascii=False)
+        
