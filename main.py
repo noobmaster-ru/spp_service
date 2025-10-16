ce0367258db011a5f6c057e17cd1a40291e14ab1
KirillKozlov <kirillandreevickozlov@yandex.ru>
2025-10-16 18:27:11 +0300

    done

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from dotenv import load_dotenv
 import os 
 
 def main():
-    parser = WBApiReportByPeriodClass(NM_ID, LIMIT_NUMBER, WB_TOKEN, URL_REPORT_DETAIL_BY_PERIOD)
+    parser = WBApiReportByPeriodClass(WB_TOKEN, URL_REPORT_DETAIL_BY_PERIOD)
     parser.parse_spp()
 
 if __name__ == "__main__":
     load_dotenv()
-    NM_ID = int(os.getenv("NM_ID_INT"))
-    LIMIT_NUMBER = int(os.getenv("LIMIT_NUMBER_INT"))
     WB_TOKEN = os.getenv("WB_TOKEN_STR")
     URL_REPORT_DETAIL_BY_PERIOD = os.getenv("URL_REPORT_DETAIL_BY_PERIOD_STR")
     main()
--- services/wb_api_report_class.py
+++ services/wb_api_report_class.py
@@ -1,22 +1,27 @@
 import requests
 import json
+from datetime import date
 
 class WBApiReportByPeriodClass():
-    def __init__(self, NM_ID: int, LIMIT_NUMBER: int, WB_TOKEN: str, URL_REPORT_DETAIL_BY_PERIOD: str):
+    def __init__(self, WB_TOKEN: str, URL_REPORT_DETAIL_BY_PERIOD: str):
         self.WB_TOKEN = WB_TOKEN
         self.URL_REPORT_DETAIL_BY_PERIOD = URL_REPORT_DETAIL_BY_PERIOD
         self.HEADERS = {
             "Authorization": self.WB_TOKEN
         }
         self.PARAMS = {
-            "limit": LIMIT_NUMBER,
-            "filterNmID": NM_ID
+            "dateFrom": date.today()
         }
     def parse_spp(self):
-        response = requests.get(self.URL_REPORT_DETAIL_BY_PERIOD, params=self.PARAMS, headers=self.HEADERS)
+        response = requests.get(self.URL_REPORT_DETAIL_BY_PERIOD, headers=self.HEADERS, params=self.PARAMS)
         if response.status_code == 200:
+            dict_nm_id_spp = {}
             data_json = response.json()
-            print(data_json)
+            for item in data_json:
+                nm_id = item["nmId"]
+                dict_nm_id_spp[nm_id] = item["spp"]
             with open("data.json", "w", encoding="utf-8") as f:
-                json.dump(data_json, f, indent=4, ensure_ascii=False)
+                json.dump(dict_nm_id_spp, f, indent=4, ensure_ascii=False)
+        else:
+            print(response.json())
         
